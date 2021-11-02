29ca24aa17cbb7414e096c6c86f931259920db3c
ShahinShanto <shahinshanto90@gmail.com>
2021-11-02 21:49:28 +0600

    Final

--- src/Component/Choose/Choose.js
+++ src/Component/Choose/Choose.js
@@ -13,31 +13,31 @@ const Choose = () => {
                                 <div className="d-flex flex-column flex-lg-row g-5 content ">
                                     <div className="col-lg-3 col-12 d-flex mb-5">
                                         <div className="text-center ">
-                                            <i className="fas fa-clinic-medical icon"></i>
+                                            <i class="fas fa-globe-europe icon"></i>
                                             <div className="counter">
-                                                <h1 className="text-followers fcolor counter ">{isVisible ? <CountUp end={756} duration={1} /> : null}</h1>
+                                                <h1 className="text-followers fcolor counter ">{isVisible ? <CountUp end={1500} duration={1} /> : null}+</h1>
                                             </div>
-                                            <h4 className="text-followers2 fcolor">Hospital Rooms</h4>
+                                            <h4 className="text-followers2 fcolor">Tour Spots</h4>
                                         </div>
                                     </div>
                                     <div className="col-lg-3 col-12 d-flex mb-5">
                                         <div className="text-center ">
-                                            <i className="fas fa-user-md icon"></i>
-                                            <h1 className="text-followers fcolor counter " >{isVisible ? <CountUp end={250} duration={1} /> : null}</h1>
-                                            <h4 className="text-followers2 fcolor">Specialist Doctors</h4>
+                                            <i class="fas fa-user icon"></i>
+                                            <h1 className="text-followers fcolor counter " >{isVisible ? <CountUp end={1000} duration={1} /> : null}+</h1>
+                                            <h4 className="text-followers2 fcolor">Tour Guides</h4>
                                         </div>
                                     </div>
                                     <div className="col-lg-3 col-12 d-flex mb-5 ">
                                         <div className="text-center">
                                             <i className="fas fa-smile-beam icon"></i>
                                             <h1 className="text-followers fcolor counter ">{isVisible ? <CountUp end={5426} duration={1} /> : null}</h1>
-                                            <h4 className="text-followers2 fcolor">Happy Patients</h4>
+                                            <h4 className="text-followers2 fcolor">Happy Clients</h4>
                                         </div>
                                     </div>
                                     <div className="col-lg-3 col-12 d-flex mb-5">
                                         <div className="text-center">
                                             <i className="far fa-calendar-alt icon"></i>
-                                            <h1 className="text-followers fcolor counter " >{isVisible ? <CountUp end={32} duration={1} /> : null}</h1>
+                                            <h1 className="text-followers fcolor counter " >{isVisible ? <CountUp end={8} duration={1} /> : null}</h1>
                                             <h4 className="text-followers2 fcolor">Years of Experience</h4>
                                         </div>
                                     </div>
